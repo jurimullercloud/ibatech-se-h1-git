40d256ae29a971d09bd80a422bdcc447e5f079dd
jurimullercloud <jurimuller.cloud@gmail.com>
2021-11-07 21:30:30 +0400

    Added warning logger in 'conflicting-branch'

--- solution.py
+++ solution.py
@@ -1,6 +1,9 @@
 import numpy as np
-
+import logging
 
 def compute(a, b):
     "Correcting documentation in function from 'conflicting-branch'"
     return a + b
+
+def log_warning(msg):
+    logging.warning(msg)
